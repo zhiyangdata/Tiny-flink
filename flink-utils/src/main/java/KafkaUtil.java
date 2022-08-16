1bfc7fb20178dd047050fa98b3049cf7269d30b0
wang sheng <wq765416999@163.com>
2022-08-16 10:09:27 +0800

    change

--- flink-utils/src/main/java/KafkaUtil.java
+++ flink-utils/src/main/java/KafkaUtil.java
@@ -1,5 +1,6 @@
 import org.apache.kafka.clients.producer.*;
 
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -18,8 +19,23 @@ import java.util.Properties;
 
 public class KafkaUtil {
 
-    static String bootstrapservers = "8.142.30.177:9092";
-    public static  void kafkaproducer(String topic,String msg)throws ExecutionException,InterruptedException {
+//    static String bootstrapservers = "8.142.30.177:9092";
+    /**
+     * @host  生产者地址： hadoop1 / hadoop2 /hadoop3
+     * @topic 主题名
+     * @msg 消息字符串
+     */
+    public void kafkaproducer(String host,String topic,String msg)throws ExecutionException,InterruptedException {
+        String bootstrapservers;
+        if(Objects.equals(host, "hadoop1")){
+            bootstrapservers = "39.99.245.209:9092";
+        }else if(Objects.equals(host, "hadoop2")){
+            bootstrapservers = "8.142.30.177:9092";
+        }else if(Objects.equals(host, "hadoop3")){
+            bootstrapservers = "39.99.245.209:9093";
+        }else{
+            throw new RuntimeException("未指定生产者host");
+        }
         Properties props = new Properties();
         //kafka 集群，broker-list
         props.put("bootstrap.servers", bootstrapservers);
@@ -37,11 +53,25 @@ public class KafkaUtil {
         //创建生产者对象
         Producer<String,String>	producer = new KafkaProducer<>(props);
         //发送数据
-        producer.send(new ProducerRecord<String, String>("first",msg));
+        producer.send(new ProducerRecord<String, String>(topic,msg));
         //关闭资源
         producer.close();
     }
-    public static void kafkaconsumer()throws ExecutionException,InterruptedException {
+    /**
+     * @host  生产者地址：hadoop1 / hadoop2 /hadoop3
+     * @topic 主题名
+     */
+    public  void kafkaconsumer(String host,String topic)throws ExecutionException,InterruptedException {
+        String bootstrapservers;
+        if(Objects.equals(host, "hadoop1")){
+            bootstrapservers = "39.99.245.209:9092";
+        }else if(Objects.equals(host, "hadoop2")){
+            bootstrapservers = "8.142.30.177:9092";
+        }else if(Objects.equals(host, "hadoop3")){
+            bootstrapservers = "39.99.245.209:9093";
+        }else{
+            throw new RuntimeException("未指定生产者host");
+        }
         //1.创建消费者配置信息
         Properties properties = new Properties();
         //链接的集群
@@ -58,10 +88,11 @@ public class KafkaUtil {
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");//重置消费者offset的方法（达到重复消费的目的），设置该属性也只在两种情况下生效：1.上面设置的消费组还未消费(可以更改组名来消费)2.latest最新、earliest也返回历史数据
         //创建生产者
         KafkaConsumer<String,String> consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(Collections.singletonList("first")); //Arrays.asList()
+        consumer.subscribe(Collections.singletonList(topic)); //Arrays.asList()
         while (true) {
             //获取数据
             ConsumerRecords<String, String> consumerRecords = consumer.poll(100);
+            // TODO: 2022/8/16  怎么消费，消费到哪里，删除以下打印代码
             //解析并打印consumerRecords
             for (ConsumerRecord consumerRecord : consumerRecords) {
                 System.out.println(consumerRecord.key() + "----" + consumerRecord.value());
