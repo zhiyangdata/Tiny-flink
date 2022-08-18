656b75a08d3b13b5d967d1388244d356042edaba
zhiyangdata <zhiyangzhu@outlook.com>
2022-08-19 04:06:12 +0800

    将JSON解析为Op类，后续须增加Op上下游之间的依赖，或者将其构建为模板类

--- flink-core/src/main/java/org/team3090/simple/Driver/Scheduler.java
+++ flink-core/src/main/java/org/team3090/simple/Driver/Scheduler.java
@@ -1,28 +1,32 @@
 package org.team3090.simple.Driver;
 
+import org.apache.zookeeper.Op;
 import org.team3090.simple.Worker.Executor;
+
+import java.util.List;
 
 /**
  * @BelongsProject: Tiny-flink
  * @BelongsPackage: org.team3090.simple.Driver
  * @Author: Zhiyang Zhu
  * @CreateTime: 2022-08-16  21:07
- * @Description: 将task发往worker的调度逻辑
+ * @Description: 将task发往worker的调度逻辑 ，内部须增加op上下游依赖的处理逻辑 @ws
  * @Version: 1.0
  */
 public class Scheduler {
-    private String[] tasks;
+    private List<Operator> tasks;
     private Executor worker;
     public Scheduler() {
-
+        worker=new Executor();
     }
 
-    public Scheduler(String[] tasks) {
+    public Scheduler(List<Operator> tasks) {
         this.tasks = tasks;
     }
     public void submitJob(){
         //向worker提交作业
-        worker.submitJob(tasks[0]);
+        tasks.stream().map(x->x.toString()).forEach(System.out::println);
+//        worker.submitJob(tasks.get(0).toString());
         //
     }
 }
